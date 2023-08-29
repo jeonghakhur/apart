30ecec16f33c1ba3f11d1e3a3446456b838cc0c9
Jeonghak hur <jeonghakhur@Jeonghakui-MacBookAir.local>
2023-08-29 20:18:36 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ export default function Footer() {
           <li>현장명 : 여의대방 트리미엄시티 (가칭 병무청역지역주택조합)</li>
           <li>현장위치 : 서울 영등포구 신길동 459-5 일원</li>
           <li>대표번호 :02-2195-3606</li>
-          <li>관리자 : 박정은 ymym9399@naver.com</li>
+          <li>
+            관리자 : 홍영기{" "}
+            <a href='mailto:gdrtfr@naver.com'>gdrtfr@naver.com</a>
+          </li>
         </ul>
         <div className='info-tel'>
           <p>분양문의</p>
